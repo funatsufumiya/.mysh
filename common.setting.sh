3809dd37c859eaec1759d266cc97e96bc9d9d13c
atmarksharp <atmarksharp@live.jp>
2015-10-07 22:32:40 +0900

    Update show-viewport

--- common.setting.sh
+++ common.setting.sh
@@ -51,9 +51,11 @@ if [ ! "$mysh_exited" ]; then
   function show-viewport(){
     echo '<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">'
     echo ''
+    echo '<style>'
     echo 'body {'
     echo '  -webkit-text-size-adjust: 100%'
     echo '}'
+    echo '</style>'
   }
 
   if test $IS_UBUNTU; then
