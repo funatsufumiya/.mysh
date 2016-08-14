d50b3980c88449c077800d31c54d11b9e999d10b
atmarksharp <atmarksharp@live.jp>
2016-08-14 15:38:07 +0900

    Updated settings

--- macbook-air-fu.setting.sh
+++ macbook-air-fu.setting.sh
@@ -1,4 +1,3 @@
-
 export PATH="/usr/local/bin:$PATH"
 export PATH="$HOME/.anyenv/bin:$PATH"
 export PATH="$HOME/bin:$PATH"
@@ -6,16 +5,25 @@ eval "$(anyenv init -)"
 
 export PATH="$HOME/app/depot_tools:$PATH"
 export PATH="$HOME/app/mirah-0.0.12/bin:$PATH"
+export PATH="$HOME/Nim/bin:$PATH"
 export PATH="$PATH:/usr/local/pear/bin"
-export PATH="$PATH:$HOME/.composer/vendor/bin/"
+export PATH="$PATH:$HOME/.composer/vendor/bin"
+export PATH="$PATH:$HOME/.nimble/bin"
+export PATH="/usr/local/opt/gettext/bin:$PATH"
+
+export PKG_CONFIG_PATH="/usr/local/lib/pkgconfig:/usr/local/share/pkgconfig"
 
 export GOPATH="$HOME/gopath/"
+export NODE_PATH="$(npm root -g 2>/dev/null)"
 
 export FFMPEG_MP3_OPTIONS="-codec:a libmp3lame -qscale:a 2"
 export ENHANCD_FILTER="fzf:peco:gof"
 
 source "$HOME/bin/antigen.zsh"
-
+alias nrpl="rlwrap nrpl"
+alias mysql-mamp="/Applications/MAMP/Library/bin/mysql"
+alias adb="/Applications/Genymotion.app/Contents/MacOS/tools/adb"
+alias utf8_to_sjis="iconv -f UTF-8 -t SHIFT-JIS"
 alias java="/Users/fu/.anyenv/envs/jenv/shims/java"
 alias macvim="open -a MacVim"
 alias "aria-x3"="aria2c --conf-path=$HOME/.aria2/x3.conf"
@@ -23,6 +31,7 @@ alias "aria-x5"="aria2c --conf-path=$HOME/.aria2/x5.conf"
 alias "aria-x7"="aria2c --conf-path=$HOME/.aria2/x7.conf"
 alias "aria-x10"="aria2c --conf-path=$HOME/.aria2/x10.conf"
 alias a="aria-x10"
+alias ip-global="wget -qO- http://ipecho.net/plain ; echo"
 
 function projectGenerator(){
   # open "$HOME/app/of_v0.8.4_osx_release/projectGenerator_osx/projectGenerator.app"
