1fe5dbe871277368fea6fcc23c6fc816d99d527c
atmarksharp <atmarksharp@live.jp>
2016-08-16 12:35:20 +0900

    Updated settings

--- acer-ubuntu-16.04.setting.sh
+++ acer-ubuntu-16.04.setting.sh
@@ -1,6 +1,10 @@
 # PATHs
 export PATH="$HOME/bin:$PATH"
 export PATH="$HOME/.anyenv/bin:$PATH"
+export PATH="$HOME/.anyenv/envs/rbenv/shims:$PATH"
+export PATH="$HOME/.anyenv/envs/pyenv/shims:$PATH"
+export PATH="$HOME/.anyenv/envs/jenv/shims:$PATH"
+export PATH="$HOME/.anyenv/envs/phpenv/shims:$PATH"
 export PATH="$PATH:$HOME/perl5/perlbrew/bin"
 
 eval "$(anyenv init - $SHELL)"
@@ -21,8 +25,15 @@ fi
 xmodmap $HOME/.keyconfig
 
 # ALIASES
+alias ls="ls --color=auto -t"
 alias ll="ls -l"
 alias la="ls -a"
+alias lla="ls -la"
 alias pbcopy='xsel --clipboard --input'
 alias pbpaste='xsel --clipboard --output'
 alias open='xdg-open &>/dev/null'
+alias less="/usr/local/share/vim/vim74/macros/less.sh"
+alias ai="sudo apt-get install"
+alias au="sudo apt-get update"
+alias as="apt-cache search"
+alias aa="sudo add-apt-repository"
