5a41b243abf982cc3c09db954b474396058a60b5
fu <fu@bunsenlabs.localhost>
2017-09-01 02:53:43 +0900

    Added hp-mini.settings.sh

---
+++ hp-mini.setting.sh
@@ -0,0 +1,5 @@
+alias "aria-x3"="aria2c --conf-path=$HOME/.aria2/x3.conf"
+alias "aria-x5"="aria2c --conf-path=$HOME/.aria2/x5.conf"
+alias "aria-x7"="aria2c --conf-path=$HOME/.aria2/x7.conf"
+alias "aria-x10"="aria2c --conf-path=$HOME/.aria2/x10.conf"
+alias "aria-x16"="aria2c --conf-path=$HOME/.aria2/x16.conf"
