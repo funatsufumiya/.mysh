d8324b1d78e8270af291f69e676733c2c4797f88
atmarksharp <atmarksharp@live.jp>
2017-09-01 03:04:59 +0900

    Added sbash alias

--- hp-mini.setting.sh
+++ hp-mini.setting.sh
@@ -3,3 +3,6 @@ alias "aria-x5"="aria2c --conf-path=$HOME/.aria2/x5.conf"
 alias "aria-x7"="aria2c --conf-path=$HOME/.aria2/x7.conf"
 alias "aria-x10"="aria2c --conf-path=$HOME/.aria2/x10.conf"
 alias "aria-x16"="aria2c --conf-path=$HOME/.aria2/x16.conf"
+
+alias "pbpaste"="xsel --clipboard --output"
+alias "sbash"="exec /bin/zsh -l"
