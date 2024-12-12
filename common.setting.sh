30cfba101579d970dc49211b823971ba587a483f
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2024-12-12 17:51:05 +0900

    fix: add zle -N

--- common.setting.sh
+++ common.setting.sh
@@ -116,6 +116,8 @@ EOF
 
     autoload -U up-line-or-beginning-search
     autoload -U down-line-or-beginning-search
+    zle -N up-line-or-beginning-search
+    zle -N down-line-or-beginning-search
 
     bindkey '^A' beginning-of-line
     bindkey '^E' end-of-line
